ce52b40351dac2e138da781aa3ad15d93e46cee5
ctrllaltdeletee <113107432+ctrllaltdeletee@users.noreply.github.com>
2026-04-17 02:39:10 +0300

    Update Gyp_11A.py

--- custom/Gyp_11A.py
+++ custom/Gyp_11A.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/dict_flow')
 def dict_flow():
     data = request.args.get('cmd')
+    key = request.args.get('keyname')
     d = {}
-    d['key'] = data
-    cmd = d['key']
+    d[key] = data
+    cmd = d[key]
     os.system(cmd)
