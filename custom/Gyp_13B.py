5c4c816b9773adcdde5f81ca28b6eb108f716945
ctrllaltdeletee <113107432+ctrllaltdeletee@users.noreply.github.com>
2026-04-21 02:06:00 +0300

    Update Gyp_13B.py

--- custom/Gyp_13B.py
+++ custom/Gyp_13B.py
@@ -1,8 +1,31 @@
-def execute_command(cmd, obj):
-    if isinstance(obj, Unsafe):
-        obj.execute(cmd)
-    else:
-        print("safe")
+from flask import Flask, request
+import os
 
-handler = get_handler(flag)
-execute_command(cmd, handler)
+app = Flask(__name__)
+
+class UnsafeHandler:
+    def execute(self, cmd):
+        os.system(cmd)
+class SafeHandler:
+    def execute(self, cmd):
+        print(f"Would execute: {cmd}")
+
+def get_handler(flag):
+    if flag:
+        return UnsafeHandler()
+    else:
+        return SafeHandler()
+
+@app.route('/cmd_isinstance')
+def cmd_isinstance():
+    cmd = request.args.get('cmd', '')
+    flag = request.args.get('flag', 'false') == 'true'
+    
+    handler = get_handler(flag)
+    
+    if isinstance(handler, UnsafeHandler):
+        handler.execute(cmd)
+    else:
+        return "Safe mode"
+    
+    return "Executed"
