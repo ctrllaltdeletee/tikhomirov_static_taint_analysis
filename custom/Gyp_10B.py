559b0ddaa797f77130564352b037416f112777e3
ctrllaltdeletee <113107432+ctrllaltdeletee@users.noreply.github.com>
2026-04-23 23:21:29 +0300

    Delete custom/Gyp_10B.py

--- custom/Gyp_10B.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/exec_dynamic')
-def exec_dynamic():
-    data = request.args.get('func')
-    exec(f"def dynamic(): return {data}")
-    return "done"
