57aa1f8153870344a29983a982a576ce9a1f9172
ctrllaltdeletee <113107432+ctrllaltdeletee@users.noreply.github.com>
2026-04-29 00:01:07 +0300

    Update Gyp_8B.py

--- custom/Gyp_8B.py
+++ custom/Gyp_8B.py
@@ -1,25 +1,14 @@
 from flask import Flask, request
-import helpers.separate_request
-import helpers.db_sqlite
+import sqlite3
+from helpers import id3
 
 app = Flask(__name__)
 
-def id1(x):
-    return x
-
-def id2(x):
-    return id1(x)
-
-def id3(x):
-    return id2(x)
-
-@app.route('/sqli_chain')
-def sqli_chain():
-    wrapped = helpers.separate_request.request_wrapper(request)
-    param = wrapped.get_form_parameter("test")
+@app.route('/sqli_identity')
+def sqli_identity():
+    param = request.args.get('name', '')
     bar = id3(param)
     sql = f"SELECT * FROM users WHERE name = '{bar}'"
-    conn = helpers.db_sqlite.get_connection()
-    cursor = conn.cursor()
-    cursor.execute(sql)
+    conn = sqlite3.connect('example.db')
+    conn.execute(sql)
     return "done"
