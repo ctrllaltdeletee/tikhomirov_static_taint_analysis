9fddca6077cbe387321fb3caee6cd95abbb1c2b1
ctrllaltdeletee <113107432+ctrllaltdeletee@users.noreply.github.com>
2026-04-28 23:59:35 +0300

    Update Gyp_1B.py

--- custom/Gyp_1B.py
+++ custom/Gyp_1B.py
@@ -5,7 +5,7 @@ from helpers import my_propagator
 app = Flask(__name__)
 
 @app.route('/test')
-def test():
+def external_sqli():
     tainted = request.args.get('id', '')
     param = my_propagator(tainted)
     conn = sqlite3.connect(':memory:')
