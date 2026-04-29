182c01e157637c7bb8df03c93de3f378aaa43462
ctrllaltdeletee <113107432+ctrllaltdeletee@users.noreply.github.com>
2026-04-29 17:22:05 +0300

    Update Gyp_6A.py

--- custom/Gyp_6A.py
+++ custom/Gyp_6A.py
@@ -2,8 +2,8 @@ from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/xss_true')
-def xss_true():
+@app.route('/xss_simple')
+def xss_simple():
     data = request.args.get('data')
     template = f"<html><body>{data}</body></html>"
     return render_template_string(template)
