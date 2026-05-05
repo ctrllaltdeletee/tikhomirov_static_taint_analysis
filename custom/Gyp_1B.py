6462f3feedaa79389312fda226f91e1811c3052c
ctrllaltdeletee <113107432+ctrllaltdeletee@users.noreply.github.com>
2026-05-06 01:16:46 +0300

    Update Gyp_1B.py

--- custom/Gyp_1B.py
+++ custom/Gyp_1B.py
@@ -4,7 +4,7 @@ from helpers import my_propagator
 
 app = Flask(__name__)
 
-@app.route('/test')
+@app.route('/external_sqli')
 def external_sqli():
     tainted = request.args.get('id', '')
     param = my_propagator(tainted)
