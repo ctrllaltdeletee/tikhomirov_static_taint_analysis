c2bb01868db79b5c7dd2a0ab647e7b948a5ac15a
ctrllaltdeletee <113107432+ctrllaltdeletee@users.noreply.github.com>
2026-05-06 01:21:20 +0300

    Update Gyp_7B.py

--- custom/Gyp_7B.py
+++ custom/Gyp_7B.py
@@ -1,7 +1,10 @@
-from flask import request
+from flask import Flask, request
 import yaml
 from helpers import OpaqueWrapper
 
+app = Flask(__name__)
+
+@app.route('/external_deserialization')
 def external_deserialization():
     param = request.headers.get("test")
     if not param:
@@ -10,5 +13,5 @@ def external_deserialization():
     wrapper = OpaqueWrapper()
     bar = wrapper.do_something(param)
 
-    yobj = yaml.load(bar, Loader=yaml.Loader)
-    return yobj
+    yaml.load(bar, Loader=yaml.Loader)
+    return "done"
